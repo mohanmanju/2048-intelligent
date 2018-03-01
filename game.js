221834b2257d47a57445549ac82af1802034b66b
mohan31 <31mohan31@gmail.com>
2018-03-01 23:19:41 +0530

    samll changes

--- game.js
+++ game.js
@@ -7,13 +7,14 @@ function setup(){
 }
 
 function draw(){
-    background(0);
-    stroke(255);
-    for(i = 1;i<4;i++){
+    background(198, 145, 79);
+    stroke(102, 85, 64);
+    strokeWeight(5);
+    for(i = 0;i<=4;i++){
         line(0,width/4*i,width,width/4*i);
         line(height/4*i,0,height/4*i,height);
     }
-
+    noStroke()
     textSize(30);
     textAlign(CENTER);
     text('4',(width/4*stepx)-width/8,(height/4*stepy)-width/8);
