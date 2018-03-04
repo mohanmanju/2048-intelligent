3228b9ec6d493ad9494efaf5e2696448dd54ae4c
mohan31 <31mohan31@gmail.com>
2018-03-04 21:55:34 +0530

    interactive

--- game.js
+++ game.js
@@ -1,9 +1,11 @@
 let stepx = 1
 let stepy = 1
+let randnumber;
 
 function setup(){
     createCanvas(400,400);
-
+    randnumber = int(random(1,3));
+    //text(str(randnumber*2),(width/4*stepx)-width/8,(height/4*stepy)-width/8);
 }
 
 function draw(){
@@ -17,11 +19,23 @@ function draw(){
     noStroke()
     textSize(30);
     textAlign(CENTER);
-    text('4',(width/4*stepx)-width/8,(height/4*stepy)-width/8);
+    text(str(randnumber*2),(width/4*stepx)-width/8,(height/4*stepy)-width/8);
 
 }
 
 function keyPressed(){
-    console.log(keyCode);
-    stepx++;
+    //console.log(stepx);
+    console.log(int(random(1,3)));
+    randnumber = int(random(1,3));
+
+    if (keyCode == LEFT_ARROW) {
+        stepx--;
+    } else if (keyCode == RIGHT_ARROW) {
+        stepx++;
+    } else if(keyCode == UP_ARROW){
+        stepy--;
+    }else if(keyCode == DOWN_ARROW){
+        stepy++;
+    }
+    //stepx++;
 }
